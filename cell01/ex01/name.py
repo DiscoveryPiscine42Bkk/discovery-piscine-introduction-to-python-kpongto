520e76dad1117d494d6bdba29d558d1d98e449b3
kpongto <gx4kong@gmail.com>
2025-06-16 07:59:06 +0000

    add name2.py

---
+++ cell01/ex01/name.py
@@ -0,0 +1,9 @@
+first_name = "Kongpob"   # ใส่ชื่อจริงของคุณ
+last_name = "Pongto"     # ใส่นามสกุลของคุณ
+
+print(first_name)
+print(last_name)
+
+whole_name = last_name + first_name
+
+print(whole_name)
