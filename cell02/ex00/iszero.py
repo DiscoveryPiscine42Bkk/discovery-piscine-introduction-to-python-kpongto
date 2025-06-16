b82e1a9d45dc55fcb83a23fed71e3121ab4cfa44
kpongto <gx4kong@gmail.com>
2025-06-16 08:20:31 +0000

    iszero.py

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,8 @@
+# ถามผู้ใช้ให้กรอกตัวเลข
+number = float(input("Please enter a number: "))
+
+# เช็คว่าตัวเลขที่กรอกเป็นศูนย์หรือไม่
+if number == 0:
+    print("This number is equal to zero.")
+else:
+    print("This number is different from zero.")
