9ac614587b5d979b8a3eb096a056ea5cad1ca83d
kpongto <gx4kong@gmail.com>
2025-06-17 02:54:41 +0000

    advanced_mult.py

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,16 @@
+
+import sys
+
+if len(sys.argv) > 1:
+    print("none")
+    sys.exit()
+
+i = 0
+while i <= 10:
+    print(f"Table de {i}:", end=" ")
+    j = 0
+    while j <= 10:
+        print(i * j, end=" " if j < 10 else "")
+        j += 1
+    print()
+    i += 1
