395930d04b97cd370823cc63346e0ed956f26c73
kpongto <gx4kong@gmail.com>
2025-06-17 06:35:50 +0000

    scope_that.py

---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def add_one(number):
+    number += 1
+
+x = 10
+print(x)        # แสดงค่าเริ่มต้นของ x
+
+add_one(x)      # เรียกฟังก์ชัน add_one
+
+print(x)        # แสดงค่า x อีกครั้ง
